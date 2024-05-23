d3159ec74d50884a532402d17c872af9531a1d16
EvATive7 <evative7@gmail.com>
2024-05-23 20:31:02 +0800

    refactor: script

--- script/generate.py
+++ script/generate.py
@@ -1,84 +1,59 @@
 from os import path
+from dataclasses import dataclass, field
+import yaml
 
-intro = {
-    '🐺': '**邶柒EvATive7**',
-    '💐': 'Enciodes SilverAsh',
-    '⚪': '「 lex parsimoniae 」'
-}
-badges = {
-    'I use': [
-        [
-            ('Spring', 'spring', '6DB33F'),
-            ('Vue', 'vuedotjs', '4FC08D'),
-            ('.NET', 'dotnet', '512BD4'),
-            ('Android', 'android', '34A853'),
-            ('Flutter', 'flutter', '02569B'),
-            ('Nginx', 'nginx', '009639'),
-            ('TensorFlow', 'tensorflow', 'FF6F00'),
-        ]
-    ],
-    'with': [
-        [
-            ('Python', 'python', '3776AB'),
-            ('JavaScript', 'javascript', 'F7DF1E'),
-            ('TypeScript', 'typescript', '3178C6'),
-            ('C%23', 'csharp', '512BD4'),
-            ('C++', 'cplusplus', '00599C'),
-            ('Java', 'oracle', 'F80000'),
-            ('Dart', 'dart', '0175C2')
-        ],
-        [
-            ('Material_Design', 'materialdesign', '757575')
-        ],
-        [
-            ('Code', 'visualstudiocode', '007ACC'),
-            ('Git', 'git', 'F05032'),
-            ('Firefox', 'firefoxbrowser', 'FF7139')
-        ],
-    ],
-    'on': [
-        [
-            ('Windows', 'windows', '0078D4'),
-            ('Ubuntu', 'ubuntu', 'E95420'),
-            ('Nvidia', 'nvidia', '76B900'),
-            ('Intel', 'intel', '0071C5')
-        ]
-    ]
-}
-footer = [
-    'Avatar ©-Ask--'
-]
-extra = ""
+config_path = path.join(path.dirname(path.realpath(__file__)), 'config.yml')
+readme_path = path.join(path.dirname(path.dirname(path.realpath(__file__))), 'README.md')
+
+
+@dataclass
+class Config:
+    introduction: dict[str, str] = field(default_factory=dict)
+    badge: dict[str, list[list[list]]] = field(default_factory=dict)
+    footer: list = field(default_factory=list)
+    extra: str = ""
+
+
+def render_nohead_table(table_data: dict[str, str]):
+    table_str = []
+    for index, (header, value) in enumerate(table_data.items()):
+        value: list[list[str]]
+        table_str.append(
+            '| {} | {} |'.format(
+                header,
+                value
+            )
+        )
+        if index == 0:
+            table_str.append('| :-- | :-- |')
+    table_str = '  \n'.join(table_str)
+    return table_str
+
+
+config = Config(**yaml.safe_load(open(config_path, 'r')))
 
 intro_str = []
-for index, value in enumerate(intro.items()):
+for index, value in enumerate(config.introduction.items()):
     intro_str.append('{} {}'.format(*value))
 intro_str = '  \n'.join(intro_str)
 
-table_str = []
-for index, (header, value) in enumerate(badges.items()):
-    value: list[list[str]]
-    table_str.append(
-        '| {} | {} |'.format(
-            header,
-            '<br>'.join(
+config.badge = {
+    header:
+    '<br>'.join(
+        [
+            ' '.join(
                 [
-                    ' '.join(
-                        [
-                            f'![](https://img.shields.io/badge/{badge[0]}-white?logo={badge[1]}&logoColor={badge[2]})'
-                            for badge in line
-                        ]
-                    )
-                    for line in value
+                    f'![](https://img.shields.io/badge/{badge[0]}-white?logo={badge[1]}&logoColor={badge[2]})'
+                    for badge in line
                 ]
             )
-        )
+            for line in value
+        ]
     )
-    if index == 0:
-        table_str.append('| :-- | :-- |')
-table_str = '  \n'.join(table_str)
+    for header, value in config.badge.items()
+}
 
-footer_str = '  \n'.join(['###### ' + _footer for _footer in footer])
+footer_str = '  \n'.join(['###### ' + _footer for _footer in config.footer])
 
 result = \
     f'''<div align="center">
@@ -86,14 +61,13 @@ result = \
 # 🥰Nice to meet you!
 {intro_str}  
 
-{extra}
+{config.extra}
 
 <div style="margin-left: auto; margin-right: auto; display: table;">
 
-{table_str}
+{render_nohead_table(config.badge)}
 </div>
 
 {footer_str}
 </div>'''
-file = path.join(path.dirname(path.dirname(path.realpath(__file__))), 'README.md')
-open(file, 'w', encoding='utf-8').write(result)
+open(readme_path, 'w', encoding='utf-8').write(result)
